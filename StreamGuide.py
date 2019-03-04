ad95bfae90e1f57bc52544e4c57f0122c4beacb5
Chris M <you@example.com>
2019-03-04 13:04:39 +0000

    Kivy Basic UI

--- StreamGuide.py
+++ StreamGuide.py
@@ -230,11 +230,11 @@ class NowTV(StreamGuide):
     def _get_submitbt(self):
         return '//*[@id="mount"]/div/div/div[2]/div[2]/section/div/section[1]/div/div/div/form/div[3]/button'
 
-
-amazon_search = Amazon()
-netflix_search = Netflix()
-nowtv_search = NowTV()
-search_str = input("What would you like to watch? \n")
-results = amazon_search.search(search_str), netflix_search.search(search_str), nowtv_search.search(search_str)
-for services in results:
-    print(services)
+if __name__ == "__main__":
+    amazon_search = Amazon()
+    netflix_search = Netflix()
+    nowtv_search = NowTV()
+    search_str = input("What would you like to watch? \n")
+    results = amazon_search.search(search_str), netflix_search.search(search_str), nowtv_search.search(search_str)
+    for services in results:
+        print(services)
